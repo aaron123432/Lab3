2f17e6e3f060b73dfb5622e18eec1aa57794d99f
aaron123432 <60013634+aaron123432@users.noreply.github.com>
2020-02-13 23:40:57 -0500

    fixing some issues
    fixing getview

---
+++ app/src/main/java/com/temple/edu/coloractivity/ColorActivity.java
@@ -0,0 +1,82 @@
+package com.temple.edu.coloractivity;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class ColorActivity extends AppCompatActivity {
+
+    Spinner spinner;
+    ArrayList<String> list;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        spinner = findViewById(R.id.spinner);
+        list.add("Red");
+        list.add("Blue");
+
+        ColorAdapter color = new ColorAdapter(this, list);
+        spinner.setAdapter(color);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+    }
+
+    class ColorAdapter extends BaseAdapter {
+
+        private ArrayList<String> list;
+        private Context context;
+
+        public ColorAdapter(Context context, ArrayList<String> list) {
+            this.context = context;
+            this.list = list;
+        }
+
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return list.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            TextView textView;
+            
+
+            return textView;
+        }
+    }
+}
